d348704d25d0040a000c2d92a634f1363ea08524
Revaycolizer <albinuslucas@gmail.com>
2024-07-28 13:01:41 +0300

    Enabled CRUD Operations by UserId  Changes to be committed: 	modified:   drizzle/meta/_journal.json 	modified:   src/index.ts 	modified:   src/schema.ts 	drizzle/0002_fine_the_call.sql 	drizzle/0003_cloudy_legion.sql 	drizzle/0004_dark_crystal.sql 	drizzle/0005_nostalgic_onslaught.sql 	drizzle/0006_bumpy_felicia_hardy.sql 	drizzle/0007_dazzling_russian.sql 	drizzle/0008_tricky_skrulls.sql 	drizzle/meta/0002_snapshot.json 	drizzle/meta/0003_snapshot.json 	drizzle/meta/0004_snapshot.json 	drizzle/meta/0005_snapshot.json 	drizzle/meta/0006_snapshot.json 	drizzle/meta/0007_snapshot.json 	drizzle/meta/0008_snapshot.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono as Todo } from 'hono'
 import { db } from '../client'
 import { products, users } from './schema'
-import { eq } from 'drizzle-orm'
+import { eq,and } from 'drizzle-orm'
 import * as jwt from 'jsonwebtoken'
 import {bearerAuth} from "hono/bearer-auth"
 const JWT_SECRET = 'your_secret_key';
@@ -104,6 +104,137 @@ app.get("/user",async(c)=>{
 })
 
 
+app.post("/create-product",async(c)=>{
+  const authHeader = c.req.header('Authorization');
+  
+  if (!authHeader || !authHeader?.startsWith('Bearer ')) {
+  return c.json({ message: 'Unauthorized' }, 401); 
+  }
+
+ 
+  const token = authHeader?.substring(7);
+
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(decoded.length <0){
+      return c.json({ message: 'Unauthorized' }, 401);
+    }
+
+     const userId = (decoded as { id: number }).id;
+
+     const user = await db.select().from(users).where(eq(users.id,userId))
+
+     if(!user){
+       return c.json({message:"User does not exist"},400)
+     }
+
+  const body = await c.req.json()
+
+  const {name,price,src} = body
+ 
+    const product = await db.insert(products).values({ name: name , price: price,src:src,userId:userId}).returning({id:products.id,name:products.name});
+  return c.json(product)
+    
+  }catch(error){
+  return c.json({message:"Internal Server Error"},500)
+  }
+  
+})
+
+app.post("/update-product",async(c)=>{
+  const authHeader = c.req.header('Authorization');
+  
+  if (!authHeader || !authHeader?.startsWith('Bearer ')) {
+  return c.json({ message: 'Unauthorized' }, 401); 
+  }
+
+ 
+  const token = authHeader?.substring(7);
+
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(decoded.length <0){
+      return c.json({ message: 'Unauthorized' }, 401);
+    }
+
+     const userId = (decoded as { id: number }).id;
+
+     const user = await db.select().from(users).where(eq(users.id,userId))
+
+     if(!user){
+       return c.json({message:"User does not exist"},400)
+     }
+
+  const body = await c.req.json()
+
+  const {id,name,price,src} = body
+
+  const Isuser = await db.select().from(products).where(and(eq(products.userId,userId),eq(products.id,id)))
+
+  if(!Isuser){
+    return c.json({message:"Unauthorized"},400)
+  }
+
+  if(!id||!name||!price||!src){
+    return c.json({message:"Please fill all fields"},400)
+  }
+ 
+    const product = await db.update(products).set({name:name,price:price,src:src}).where(eq(products.id, id)).returning({id:products.id,name:products.name,price:products.price});
+  return c.json(product)
+    
+  }catch(error){
+  return c.json({message:"Internal Server Error"},500)
+  }
+  
+})
+
+
+app.post("/delete-product/:id",async(c)=>{
+  const authHeader = c.req.header('Authorization');
+  
+  if (!authHeader || !authHeader?.startsWith('Bearer ')) {
+  return c.json({ message: 'Unauthorized' }, 401); 
+  }
+
+ 
+  const token = authHeader?.substring(7);
+
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(decoded.length <0){
+      return c.json({ message: 'Unauthorized' }, 401);
+    }
+
+     const userId = (decoded as { id: number }).id;
+
+     const user = await db.select().from(users).where(eq(users.id,userId))
+
+     if(!user){
+       return c.json({message:"User does not exist"},400)
+     }
+
+  const id = c.req.param('id')
+
+ if(!id){
+  return c.json({message:"Please fill all fields"},400)
+ }
+
+  const Isuser = await db.select().from(products).where(and(eq(products.userId,userId),eq(products.id,id as any)));
+
+  if(!Isuser){
+    return c.json({message:"Unauthorized"},400)
+  }
+
+  return c.json({message:"Deleted successfully"});
+    
+  }catch(error){
+  return c.json({message:"Internal Server Error"},500)
+  }
+  
+})
 
 app.get('/products',async(c)=>{
   const product = await db.select().from(products);
--- src/schema.ts
+++ src/schema.ts
@@ -2,16 +2,16 @@ import { password } from "bun";
 import { bigint, numeric, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
 
-export const users = pgTable('user',{
-    id:serial('id').primaryKey(),
+export const users = pgTable('users',{
+    id:bigint('id',{mode:'number'}).primaryKey().generatedByDefaultAsIdentity(),
     name:text('name'),
     password:varchar('password')
 })
 
-export const products = pgTable('product',{
-    id:serial('id').primaryKey(),
+export const products = pgTable('items',{
+    id:bigint('id',{mode:'number'}).primaryKey().generatedByDefaultAsIdentity(),
     name:text('name'),
     price:bigint('price',{mode:'number'}),
     src:text('src'),
-    userId:serial('userId').references(()=>users.id)
+    userId:bigint('userId',{mode:'number'}).references(()=>users.id)
 })
